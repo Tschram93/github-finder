68d59696a1c31a48102b2211f4cbea6d01c19da6
TS93 <tschram93@yahoo.com>
2022-01-07 15:38:28 -0500

    converted app function into a class base

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>
-          Using React
-      </h1>
-    </div>
-  );
+class App extends Component {
+	render() {
+		return (
+			<div className='App'>
+				<h1>Using React</h1>
+			</div>
+		);
+	}
 }
 
 export default App;
