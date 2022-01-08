aa52001fc0e8b534067834ec9d07fcf9fccd5b6a
TS93 <tschram93@yahoo.com>
2022-01-08 12:52:21 -0500

    implemented loading gif into users file; utilized proptype

--- src/components/layout/Spinner.js
+++ src/components/layout/Spinner.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import spinner from './spinner.gif';
 
-export const Spinner = () => {
-        <Fragment>
-            <img src={spinner} alt='Loading...' style={{ width: '200px', margin: 'auto', display: 'block' }} />
-        </Fragment>
-}
+const Spinner = () => (
+	<Fragment>
+		<img
+			src={spinner}
+			alt='Loading...'
+			style={{ width: '200px', margin: 'auto', display: 'block' }}
+		/>
+	</Fragment>
+);
+
+export default Spinner;
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,9 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import UserItem from './UserItem';
+import Spinner from '../layout/Spinner';
 
-class Users extends Component {
-	render() {
-		const { users } = this.props;
+
+const Users = ({ users, loading }) => {
+	if(loading) {
+		return <Spinner />
+	} else {
 		return (
 			<div style={userStyle}>
 				{users.map((user) => (
@@ -12,6 +16,12 @@ class Users extends Component {
 			</div>
 		);
 	}
+}
+
+Users.propTypes = {
+	users = PropTypes.array.isRequired,
+	users = PropTypes.bool.isRequired,
+	
 }
 
 const userStyle = {
