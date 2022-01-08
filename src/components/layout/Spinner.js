0451ada346af12d1080f4f5d9b339a1e3e6bb1fe
TS93 <tschram93@yahoo.com>
2022-01-08 12:28:26 -0500

    uploaded spinner GIF, and implemented into spinner.js for when users are loading

---
+++ src/components/layout/Spinner.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import spinner from './spinner.gif';
+
+export const Spinner = () => {
+        <Fragment>
+            <img src={spinner} alt='Loading...' style={{ width: '200px', margin: 'auto', display: 'block' }} />
+        </Fragment>
+}
