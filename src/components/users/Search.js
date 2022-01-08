ba85d199ec96b93f545a6ecde9e410cf92d429f6
TS93 <tschram93@yahoo.com>
2022-01-08 13:33:23 -0500

    events added into search(button)

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
 
 export class Search extends Component {
-    render() {
-        return (
-            <div>
-                <form className='form'>
-                    <input type="text" name="text" placeholder="Search Users..." />
-                    <input type="submit" value="Search" className='btn btn-dark btn-block' />
-                </form>
-            </div>
-        )
+	state = {
+		text: '',
+	};
+
+    onChange = (e) => {
+        this.setState({ [e.target.name]: e.target.value });
     }
+
+    onSubmit = (e) => {
+        e.preventDefault();
+    }
+
+	render() {
+		return (
+			<div>
+				<form onSubmit={this.onSubmit} className='form'>
+					<input type='text' name='text' placeholder='Search Users...' />
+					<input
+						type='submit'
+						value={this.state.text}
+						className='btn btn-dark btn-block'
+                        onChange={this.onChange}
+					/>
+				</form>
+			</div>
+		);
+	}
 }
 
 export default Search;
