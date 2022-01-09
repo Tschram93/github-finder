0afaa6b8608016a926a81f5fd7c73d8daa0f47b7
TS93 <tschram93@yahoo.com>
2022-01-09 12:24:35 -0500

    clear function created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import axios from 'axios';
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
-import PropTypes from 'prop-types';
-import Search from './components/users/Search';
 import Users from './components/users/Users';
+import Search from './components/users/Search';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
@@ -12,38 +11,33 @@ class App extends Component {
 		loading: false,
 	};
 
-	static PropTypes = {
-		searchUsers: PropTypes.func.isRequired
-	};
-
-	// async componentDidMount() {
-	// 	this.setState({ loading: true });
-
-	// 	const res = await axios.get(
-	// 		`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-	// 	);
-
-	// 	this.setState({ users: res.data, loading: false });
-	// }
-
-	// Search Github Users
+	// Search Github users
 	searchUsers = async (text) => {
 		this.setState({ loading: true });
 
 		const res = await axios.get(
-			`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+			`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
 		);
 
 		this.setState({ users: res.data.items, loading: false });
 	};
 
+	// Clear users from state
+	clearUsers = () => this.setState({ users: [], loading: false });
+
 	render() {
+		const { users, loading } = this.state;
+
 		return (
 			<div className='App'>
-				<Navbar title='Github Finder' icon='fab fa-github' />
+				<Navbar />
 				<div className='container'>
-					<Search searchUsers={this.searchUsers} />
-					<Users loading={this.state.loading} users={this.state.users} />
+					<Search
+						searchUsers={this.searchUsers}
+						clearUsers={this.clearUsers}
+						showClear={users.length > 0 ? true : false}
+					/>
+					<Users loading={loading} users={users} />
 				</div>
 			</div>
 		);
